20242579c07021d4cf2873e0403ee17195ee14bf
bmhaskar <bmhaskar@gmail.com>
2019-10-10 19:21:20 +0530

    Updated defaultMeta as per suggetions. Removed the logging to file from default configuration.

--- src/logger.ts
+++ src/logger.ts
@@ -1,24 +1,14 @@
 import * as winston from "winston";
 import { Logger } from "winston";
-import * as path from "path";
 import { LOGGER_LEVEL } from "./config";
-
-const errorFilePath = path.resolve("..", "logs", "error.logs");
-const combinedErrorFilePath = path.resolve("..", "logs", "combined.logs");
 
 const container = new winston.Container();
 
 const winstonOptions = {
   level: LOGGER_LEVEL,
   format: winston.format.json(),
-  defaultMeta: { "worker-id": "dfault-graphile-worker" },
+  defaultMeta: { "worker-id": "graphile-worker" },
   transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new winston.transports.File({ filename: errorFilePath, level: "error" }),
-    new winston.transports.File({ filename: combinedErrorFilePath }),
     new winston.transports.Console({
       format: winston.format.simple(),
     }),
