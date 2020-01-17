ead5d3d8c6ec3e65f638c8c25dc0c1e56a62fb90
Greg Brown <greg@gregbrown.co.nz>
2020-01-17 21:19:56 +1300

    Update perfTest/recreateDb.js
    Co-Authored-By: Benjie Gillam <benjie@jemjie.com>

--- perfTest/recreateDb.js
+++ perfTest/recreateDb.js
@@ -1,6 +1,9 @@
 const { Pool } = require("pg");
 const { parse } = require("pg-connection-string");
 
+if (!process.env.PERF_DATABASE_URL) {
+  throw new Error("No PERF_DATABASE_URL setting detected; please don't call this script directly!");
+}
 const config = parse(process.env.PERF_DATABASE_URL);
 // don't connect to the provided db, or we can't drop it
 const pgPool = new Pool({ ...config, database: "template1" });
