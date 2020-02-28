acbb088117bb5241d001269ee07e20f09cf8c8b5
Benjie Gillam <benjie@jemjie.com>
2020-02-28 15:10:57 +0000

    Test refactoring

--- __tests__/workerUtils.rescheduleJobs.test.ts
+++ __tests__/workerUtils.rescheduleJobs.test.ts
@@ -1,4 +1,9 @@
-import { withPgClient, reset, TEST_CONNECTION_STRING } from "./helpers";
+import {
+  withPgClient,
+  reset,
+  TEST_CONNECTION_STRING,
+  makeSelectionOfJobs,
+} from "./helpers";
 import { makeWorkerUtils, Job } from "../src/index";
 
 /** For sorting arrays of numbers or numeric strings */
@@ -14,27 +19,18 @@ test("completes the jobs, leaves others unaffected", () =>
     const utils = await makeWorkerUtils({
       connectionString: TEST_CONNECTION_STRING,
     });
-    const future = new Date(Date.now() + 60 * 60 * 1000);
-    let failedJob = await utils.addJob("job1", { a: 1, runAt: future });
-    const regularJob1 = await utils.addJob("job1", { a: 2, runAt: future });
-    let lockedJob = await utils.addJob("job1", { a: 3, runAt: future });
-    const regularJob2 = await utils.addJob("job1", { a: 4, runAt: future });
-    const untouchedJob = await utils.addJob("job1", { a: 5, runAt: future });
-    ({
-      rows: [lockedJob],
-    } = await pgClient.query<Job>(
-      `update graphile_worker.jobs set locked_by = 'test', locked_at = now() where id = $1 returning *`,
-      [lockedJob.id]
-    ));
-    ({
-      rows: [failedJob],
-    } = await pgClient.query<Job>(
-      `update graphile_worker.jobs set attempts = max_attempts, last_error = 'Failed forever' where id = $1 returning *`,
-      [failedJob.id]
-    ));
+
+    const {
+      failedJob,
+      regularJob1,
+      lockedJob,
+      regularJob2,
+      untouchedJob,
+    } = await makeSelectionOfJobs(utils, pgClient);
 
     const jobs = [failedJob, regularJob1, lockedJob, regularJob2];
-    const jobIds = jobs.map(j => j.id).sort(numerically);
+    const jobIds = jobs.map(j => j.id);
+
     const nowish = new Date(Date.now() + 60000);
     const rescheduledJobs = await utils.rescheduleJobs(jobIds, {
       runAt: nowish,
@@ -53,9 +49,7 @@ test("completes the jobs, leaves others unaffected", () =>
     }
 
     // Assert that it has an entry in jobs / job_queues
-    const {
-      rows: remaining,
-    } = await pgClient.query(
+    const { rows: remaining } = await pgClient.query<Job>(
       `select * from graphile_worker.jobs where not (id = any($1)) order by id asc`,
       [rescheduledJobIds]
     );
