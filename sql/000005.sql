60da79aa3f5c10e83afb05abeb9af0fd720307b1
Luke R. Ramsden <lukeramsden@protonmail.com>
2020-08-21 15:01:51 +0100

    feat: track revision count for jobs (#130)
    Co-authored-by: Benjie Gillam <benjie@jemjie.com>

---
+++ sql/000005.sql
@@ -0,0 +1,104 @@
+alter table :GRAPHILE_WORKER_SCHEMA.jobs add column revision int default 0 not null;
+
+create or replace function :GRAPHILE_WORKER_SCHEMA.add_job(
+  identifier text,
+  payload json = null,
+  queue_name text = null,
+  run_at timestamptz = null,
+  max_attempts int = null,
+  job_key text = null,
+  priority int = null
+) returns :GRAPHILE_WORKER_SCHEMA.jobs as $$
+declare
+  v_job :GRAPHILE_WORKER_SCHEMA.jobs;
+begin
+  -- Apply rationality checks
+  if length(identifier) > 128 then
+    raise exception 'Task identifier is too long (max length: 128).' using errcode = 'GWBID';
+  end if;
+  if queue_name is not null and length(queue_name) > 128 then
+    raise exception 'Job queue name is too long (max length: 128).' using errcode = 'GWBQN';
+  end if;
+  if job_key is not null and length(job_key) > 512 then
+    raise exception 'Job key is too long (max length: 512).' using errcode = 'GWBJK';
+  end if;
+  if max_attempts < 1 then
+    raise exception 'Job maximum attempts must be at least 1' using errcode = 'GWBMA';
+  end if;
+
+  if job_key is not null then
+    -- Upsert job
+    insert into :GRAPHILE_WORKER_SCHEMA.jobs (
+      task_identifier,
+      payload,
+      queue_name,
+      run_at,
+      max_attempts,
+      key,
+      priority
+    )
+      values(
+        identifier,
+        coalesce(payload, '{}'::json),
+        queue_name,
+        coalesce(run_at, now()),
+        coalesce(max_attempts, 25),
+        job_key,
+        coalesce(priority, 0)
+      )
+      on conflict (key) do update set
+        task_identifier=excluded.task_identifier,
+        payload=excluded.payload,
+        queue_name=excluded.queue_name,
+        max_attempts=excluded.max_attempts,
+        run_at=excluded.run_at,
+        priority=excluded.priority,
+        revision=jobs.revision + 1,
+
+        -- always reset error/retry state
+        attempts=0,
+        last_error=null
+      where jobs.locked_at is null
+      returning *
+      into v_job;
+
+    -- If upsert succeeded (insert or update), return early
+    if not (v_job is null) then
+      return v_job;
+    end if;
+
+    -- Upsert failed -> there must be an existing job that is locked. Remove
+    -- existing key to allow a new one to be inserted, and prevent any
+    -- subsequent retries by bumping attempts to the max allowed.
+    update :GRAPHILE_WORKER_SCHEMA.jobs
+      set
+        key = null,
+        attempts = jobs.max_attempts
+      where key = job_key;
+  end if;
+
+  -- insert the new job. Assume no conflicts due to the update above
+  insert into :GRAPHILE_WORKER_SCHEMA.jobs(
+    task_identifier,
+    payload,
+    queue_name,
+    run_at,
+    max_attempts,
+    key,
+    priority
+  )
+    values(
+      identifier,
+      coalesce(payload, '{}'::json),
+      queue_name,
+      coalesce(run_at, now()),
+      coalesce(max_attempts, 25),
+      job_key,
+      coalesce(priority, 0)
+    )
+    returning *
+    into v_job;
+
+  return v_job;
+end;
+$$ language plpgsql volatile;
