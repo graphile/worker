1d7a2549c88b76d2b7d4b1775ab87d03d6656c57
Benjie Gillam <benjie@jemjie.com>
2020-12-18 20:05:23 +0000

    More verbose logging

--- perfTest/n.js
+++ perfTest/n.js
@@ -1,6 +1,6 @@
 const a = 3;
 exports.a = a;
 const b = require("./b").b;
-const n = a + b;
+const n = a + b; /* 3 + 5 */
 exports.n = n;
-console.log(`n is ${n}`);
+console.log(`perfTest/n.js cyclic check: n is ${n} (expect 8)`);
