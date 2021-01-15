4070c240d797a4ce4661e59703934940304db1d0
Benjie Gillam <benjie@jemjie.com>
2021-01-15 11:28:24 +0000

    Tests for clock skew

--- __tests__/cron-timing.test.ts
+++ __tests__/cron-timing.test.ts
@@ -1,8 +1,9 @@
-import { run } from "../src";
+import { run, Runner } from "../src";
 import {
   EventMonitor,
   getJobs,
   HOUR,
+  MINUTE,
   reset,
   SECOND,
   setupFakeTimers,
@@ -12,6 +13,16 @@ import {
 
 const { setTime } = setupFakeTimers();
 const REFERENCE_TIMESTAMP = 1609459200000; /* 1st January 2021, 00:00:00 UTC */
+
+// Even on test fail we need the runner to shut down, so clean up after each test (rather than during).
+let runner: null | Runner = null;
+afterEach(() => {
+  if (runner) {
+    const promise = runner.stop();
+    runner = null;
+    return promise;
+  }
+});
 
 test("check timestamp is correct", () => {
   setTime(REFERENCE_TIMESTAMP);
@@ -27,7 +38,7 @@ test("executes job when expected", () =>
     const cronFinishedBackfilling = eventMonitor.awaitNext("cron:started");
     const poolReady = eventMonitor.awaitNext("pool:listen:success");
     const cronScheduleCalls = eventMonitor.count("cron:schedule");
-    const runner = await run({
+    runner = await run({
       ...options,
       crontab: `0 */4 * * * my_task`,
       events: eventMonitor.events,
@@ -40,7 +51,7 @@ test("executes job when expected", () =>
     expect(cronScheduleCalls.count).toEqual(0);
 
     const cronScheduleComplete = eventMonitor.awaitNext("cron:scheduled");
-    await setTime(REFERENCE_TIMESTAMP + 4 * HOUR + 30 * SECOND); // 4:00:01am
+    await setTime(REFERENCE_TIMESTAMP + 4 * HOUR + 1 * SECOND); // 4:00:01am
     expect(cronScheduleCalls.count).toEqual(1);
 
     const { timestamp, jobsAndIdentifiers } = cronScheduleCalls.lastEvent!;
@@ -52,10 +63,79 @@ test("executes job when expected", () =>
     await cronScheduleComplete;
     await sleep(50);
 
-    await runner.stop();
-
     {
       const jobs = await getJobs(pgPool);
       expect(jobs).toHaveLength(1);
     }
+  }));
+
+test("doesn't schedule tasks twice when system clock reverses", () =>
+  withOptions(async (options) => {
+    await setTime(REFERENCE_TIMESTAMP + HOUR); // 1am
+    const { pgPool } = options;
+    await reset(pgPool, options);
+    const eventMonitor = new EventMonitor();
+    const cronFinishedBackfilling = eventMonitor.awaitNext("cron:started");
+    const poolReady = eventMonitor.awaitNext("pool:listen:success");
+    const cronScheduleCalls = eventMonitor.count("cron:schedule");
+    runner = await run({
+      ...options,
+      crontab: `0 */4 * * * my_task`,
+      events: eventMonitor.events,
+    });
+    await cronFinishedBackfilling;
+    await poolReady;
+    expect(cronScheduleCalls.count).toEqual(0);
+
+    await setTime(REFERENCE_TIMESTAMP + 3 * HOUR + 1 * SECOND); // 3:00:01am
+    expect(cronScheduleCalls.count).toEqual(0);
+
+    await setTime(REFERENCE_TIMESTAMP + 4 * HOUR + 1 * SECOND); // 4:00:01am
+    expect(cronScheduleCalls.count).toEqual(1);
+
+    // REWIND TIME!
+    await setTime(REFERENCE_TIMESTAMP + 3 * HOUR + 1 * SECOND);
+    // Advance time again
+    await setTime(REFERENCE_TIMESTAMP + 4 * HOUR + 1 * SECOND);
+    // Although the time was matched again, no tasks should have been scheduled
+    expect(cronScheduleCalls.count).toEqual(1);
+  }));
+
+test("clock skew doesn't prevent task from being scheduled at the right time", () =>
+  withOptions(async (options) => {
+    await setTime(REFERENCE_TIMESTAMP + HOUR); // 1am
+    const { pgPool } = options;
+    await reset(pgPool, options);
+    const eventMonitor = new EventMonitor();
+    const cronFinishedBackfilling = eventMonitor.awaitNext("cron:started");
+    const poolReady = eventMonitor.awaitNext("pool:listen:success");
+    const cronScheduleCalls = eventMonitor.count("cron:schedule");
+    runner = await run({
+      ...options,
+      crontab: `0 */4 * * * my_task`,
+      events: eventMonitor.events,
+    });
+    await cronFinishedBackfilling;
+    await poolReady;
+    expect(cronScheduleCalls.count).toEqual(0);
+
+    await setTime(REFERENCE_TIMESTAMP + 3 * HOUR + 1 * SECOND); // 3:00:01am
+    expect(cronScheduleCalls.count).toEqual(0);
+
+    // Advance time
+    await setTime(REFERENCE_TIMESTAMP + 4 * HOUR - 30 * SECOND); // 3:59:30am
+    expect(cronScheduleCalls.count).toEqual(0);
+
+    // Jump back and forward a few times
+    for (let i = 0; i < 10; i++) {
+      await setTime(REFERENCE_TIMESTAMP + 4 * HOUR - 1 * MINUTE); // 3:59:00am
+      expect(cronScheduleCalls.count).toEqual(0);
+
+      await setTime(REFERENCE_TIMESTAMP + 4 * HOUR - 30 * SECOND); // 3:59:30am
+      expect(cronScheduleCalls.count).toEqual(0);
+    }
+
+    // Finally advane the clock to cron firing
+    await setTime(REFERENCE_TIMESTAMP + 4 * HOUR + 1 * SECOND); // 4:00:01am
+    expect(cronScheduleCalls.count).toEqual(1);
   }));
