028f6c8bf0a940d2c237f60ff8c053197ced848b
Benjie Gillam <benjie@jemjie.com>
2021-03-29 11:58:50 +0100

    Don't kill worker on SIGPIPE

--- src/signals.ts
+++ src/signals.ts
@@ -2,7 +2,7 @@ export type Signal =
   | "SIGUSR2"
   | "SIGINT"
   | "SIGTERM"
-  | "SIGPIPE"
+  // | "SIGPIPE"
   | "SIGHUP"
   | "SIGABRT";
 
@@ -10,7 +10,17 @@ export default [
   "SIGUSR2",
   "SIGINT",
   "SIGTERM",
-  "SIGPIPE",
+  /*
+   * Though SIGPIPE is a terminal signal _normally_, this isn't the case for
+   * Node.js since libuv handles it. From the Node docs:
+   *
+   * > 'SIGPIPE' is ignored by default. It can have a listener installed.
+   * > -- https://nodejs.org/api/process.html
+   *
+   * We don't want the process to exit on SIGPIPE, so we ignore it (and rely on
+   * Node.js to handle it through the normal error channels).
+   */
+  // "SIGPIPE",
   "SIGHUP",
   "SIGABRT",
 ] as Array<Signal>;
