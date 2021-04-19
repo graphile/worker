2e901637d77c1b43b28a4a8ac52e666bce443f47
Nirmal Benny <nirmal@syscloud.org>
2021-04-19 10:34:50 +0530

    getCronItems function exposed from index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import getTasks from "./getTasks";
+import getCronItems from "./getCronItems";
 export { parseCronItems, parseCrontab } from "./crontab";
 export * from "./interfaces";
 export { consoleLogFactory, LogFunctionFactory, Logger } from "./logger";
@@ -7,3 +8,4 @@ export { run, runMigrations, runOnce } from "./runner";
 export { makeWorkerUtils, quickAddJob } from "./workerUtils";
 
 export { getTasks };
+export {getCronItems};
