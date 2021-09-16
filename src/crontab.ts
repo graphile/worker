e162f7622e63702dd5ab7791bf2cbd1758c3529f
Benjie Gillam <benjie@jemjie.com>
2021-09-16 12:15:44 +0100

    chore(refactor): split some cron code into new files to ease maintenance (#220)

--- src/crontab.ts
+++ src/crontab.ts
@@ -1,156 +1,20 @@
 import * as JSON5 from "json5";
 import { parse } from "querystring";
 
+import {
+  CRONTAB_COMMAND,
+  CRONTAB_LINE_PARTS,
+  CRONTAB_OPTIONS_BACKFILL,
+  CRONTAB_OPTIONS_ID,
+  CRONTAB_OPTIONS_MAX,
+  CRONTAB_OPTIONS_PRIORITY,
+  CRONTAB_OPTIONS_QUEUE,
+  CRONTAB_TIME_PARTS,
+  PERIOD_DURATIONS,
+  TIMEPHRASE_PART,
+} from "./cronConstants";
+import { parseCrontabRanges } from "./cronMatcher";
 import { CronItem, CronItemOptions, ParsedCronItem } from "./interfaces";
-
-/** One second in milliseconds */
-const SECOND = 1000;
-/** One minute in milliseconds */
-const MINUTE = 60 * SECOND;
-/** One hour in milliseconds */
-const HOUR = 60 * MINUTE;
-/** One day in milliseconds */
-const DAY = 24 * HOUR;
-/** One week in milliseconds */
-const WEEK = 7 * DAY;
-
-// A (non-comment, non-empty) line in the crontab file
-/** Separates crontab line into the minute, hour, day of month, month, day of week and command parts. */
-const CRONTAB_LINE_PARTS = /^([0-9*/,-]+)\s+([0-9*/,-]+)\s+([0-9*/,-]+)\s+([0-9*/,-]+)\s+([0-9*/,-]+)\s+(.*)$/;
-/** Just the time expression from CRONTAB_LINE_PARTS */
-const CRONTAB_TIME_PARTS = /^([0-9*/,-]+)\s+([0-9*/,-]+)\s+([0-9*/,-]+)\s+([0-9*/,-]+)\s+([0-9*/,-]+)$/;
-
-// Crontab ranges from the minute, hour, day of month, month and day of week parts of the crontab line
-/** Matches an explicit numeric value */
-const CRONTAB_NUMBER = /^([0-9]+)$/;
-/** Matches a range of numeric values */
-const CRONTAB_RANGE = /^([0-9]+)-([0-9]+)$/;
-/** Matches a numeric wildcard, optionally with a divisor */
-const CRONTAB_WILDCARD = /^\*(?:\/([0-9]+))?$/;
-
-// The command from the crontab line
-/** Splits the command from the crontab line into the task, options and payload. */
-const CRONTAB_COMMAND = /^([_a-zA-Z][_a-zA-Z0-9:_-]*)(?:\s+\?([^\s]+))?(?:\s+(\{.*\}))?$/;
-
-// Crontab command options
-/** Matches the id=UID option, capturing the unique identifier */
-const CRONTAB_OPTIONS_ID = /^([_a-zA-Z][-_a-zA-Z0-9]*)$/;
-/** Matches the fill=t option, capturing the time phrase  */
-const CRONTAB_OPTIONS_BACKFILL = /^((?:[0-9]+[smhdw])+)$/;
-/** Matches the max=n option, capturing the max executions number */
-const CRONTAB_OPTIONS_MAX = /^([0-9]+)$/;
-/** Matches the queue=name option, capturing the queue name */
-const CRONTAB_OPTIONS_QUEUE = /^([-a-zA-Z0-9_:]+)$/;
-/** Matches the priority=n option, capturing the priority value */
-const CRONTAB_OPTIONS_PRIORITY = /^(-?[0-9]+)$/;
-
-/**
- * Parses a range from a crontab line; a comma separated list of:
- *
- * - exact number
- * - wildcard `*` optionally with `/n` divisor
- * - range `a-b`
- *
- * Returns an ordered list of unique numbers in the range `min` to `max` that match the given range.
- *
- * If `wrap` is true, then the number `max + 1` will be replaced by the number
- * `min`; this is specifically to handle the value `7` being used to represent
- * Sunday (as opposed to `0` which is correct).
- */
-const parseCrontabRange = (
-  locationForError: string,
-  range: string,
-  min: number,
-  max: number,
-  wrap = false,
-): number[] => {
-  const parts = range.split(",");
-  const numbers: number[] = [];
-
-  /**
-   * Adds a number to our numbers array after wrapping it (if necessary) and
-   * checking it's in the valid range.
-   */
-  function add(number: number) {
-    const wrappedNumber = wrap && number === max + 1 ? min : number;
-    if (wrappedNumber > max) {
-      throw new Error(
-        `Too large value '${number}' in ${locationForError}: expected values in the range ${min}-${max}.`,
-      );
-    } else if (wrappedNumber < min) {
-      throw new Error(
-        `Too small value '${number}' in ${locationForError}: expected values in the range ${min}-${max}.`,
-      );
-    } else {
-      numbers.push(wrappedNumber);
-    }
-  }
-
-  for (const part of parts) {
-    {
-      const matches = CRONTAB_NUMBER.exec(part);
-      if (matches) {
-        add(parseInt(matches[1], 10));
-        continue;
-      }
-    }
-    {
-      const matches = CRONTAB_RANGE.exec(part);
-      if (matches) {
-        const a = parseInt(matches[1], 10);
-        const b = parseInt(matches[2], 10);
-        if (b <= a) {
-          throw new Error(
-            `Invalid range '${part}' in ${locationForError}: destination is not larger than source`,
-          );
-        }
-        for (let i = a; i <= b; i++) {
-          add(i);
-        }
-        continue;
-      }
-    }
-    {
-      const matches = CRONTAB_WILDCARD.exec(part);
-      if (matches) {
-        const divisor = matches[1] ? parseInt(matches[1], 10) : 1;
-        if (divisor >= 1) {
-          for (let i = min; i <= max; i += divisor) {
-            // We know this is fine, so no need to call `add`
-            numbers.push(i);
-          }
-        } else {
-          throw new Error(
-            `Invalid wildcard expression '${part}' in ${locationForError}: divisor '${matches[1]}' expected to be greater than zero`,
-          );
-        }
-        continue;
-      }
-    }
-    throw new Error(
-      `Unsupported syntax '${part}' in ${locationForError}: this doesn't appear to be a number, range or wildcard`,
-    );
-  }
-
-  numbers.sort((a, b) => a - b);
-
-  // Filter out numbers that are identical to the previous number
-  const uniqueNumbers = numbers.filter(
-    (currentNumber, idx) => idx === 0 || numbers[idx - 1] !== currentNumber,
-  );
-
-  return uniqueNumbers;
-};
-
-/** Matches the quantity and period string at the beginning of a timephrase */
-const TIMEPHRASE_PART = /^([0-9]+)([smhdw])/;
-const PERIOD_DURATIONS = {
-  s: SECOND,
-  m: MINUTE,
-  h: HOUR,
-  d: DAY,
-  w: WEEK,
-};
 
 /**
  * Returns a period of time in milliseconds representing the time phrase given.
@@ -307,41 +171,6 @@ const parseCrontabCommand = (
   const payload = parseCrontabPayload(lineNumber, payloadString);
   return { task, options, payload, identifier };
 };
-
-function parseCrontabRanges(matches: string[], source: string) {
-  const minutes = parseCrontabRange(
-    `minutes range in ${source}`,
-    matches[1],
-    0,
-    59,
-  );
-  const hours = parseCrontabRange(
-    `hours range in ${source}`,
-    matches[2],
-    0,
-    23,
-  );
-  const dates = parseCrontabRange(
-    `dates range in ${source}`,
-    matches[3],
-    1,
-    31,
-  );
-  const months = parseCrontabRange(
-    `months range in ${source}`,
-    matches[4],
-    1,
-    12,
-  );
-  const dows = parseCrontabRange(
-    `days of week range in ${source}`,
-    matches[5],
-    0,
-    6,
-    true,
-  );
-  return { minutes, hours, dates, months, dows };
-}
 
 /**
  * Parses a line from a crontab file, such as `* * * * * my_task`
