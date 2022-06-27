e7dd7d89070a43993ac5252f6ef83a22101efcbf
Benjie Gillam <benjie@jemjie.com>
2022-06-27 14:24:31 +0100

    Move get_job into JS

--- sql/000011.sql
+++ sql/000011.sql
@@ -1,2 +1,3 @@
 drop function graphile_worker.complete_job;
 drop function graphile_worker.fail_job;
+drop function graphile_worker.get_job;
--- src/sql/getJob.ts
+++ src/sql/getJob.ts
@@ -15,20 +15,65 @@ export async function getJob(
     options: { noPreparedStatements },
   } = compiledSharedOptions;
 
+  const now = useNodeTime ? "$4::timestamptz" : "now()";
+
   const {
     rows: [jobRow],
   } = await withPgClient((client) =>
     // TODO: breaking change; change this to more optimal:
     // `SELECT id, queue_name, task_identifier, payload FROM ...`,
     client.query<Job>({
-      text: `SELECT * FROM ${escapedWorkerSchema}.get_job($1, $2, forbidden_flags := $3::text[], now := coalesce($4::timestamptz, now()));`,
+      text: `\
+with j as (
+  select jobs.queue_name, jobs.id
+    from ${escapedWorkerSchema}.jobs
+    where (jobs.locked_at is null or jobs.locked_at < (${now} - interval '4 hours'))
+    and (
+      jobs.queue_name is null
+    or
+      exists (
+        select 1
+        from ${escapedWorkerSchema}.job_queues
+        where job_queues.queue_name = jobs.queue_name
+        and (job_queues.locked_at is null or job_queues.locked_at < (${now} - interval '4 hours'))
+        for update
+        skip locked
+      )
+    )
+    and run_at <= ${now}
+    and attempts < max_attempts
+    and ($2::text[] is null or task_identifier = any($2::text[]))
+    and ($3::text[] is null or (flags ?| $3::text[]) is not true)
+    order by priority asc, run_at asc, id asc
+    limit 1
+    for update
+    skip locked
+),
+q as (
+  update ${escapedWorkerSchema}.job_queues
+    set
+      locked_by = $1::text,
+      locked_at = ${now}
+    from j
+    where job_queues.queue_name = j.queue_name
+)
+  update ${escapedWorkerSchema}.jobs
+    set
+      attempts = jobs.attempts + 1,
+      locked_by = $1::text,
+      locked_at = ${now}
+    from j
+    where jobs.id = j.id
+    returning *`,
       values: [
         workerId,
         supportedTaskNames,
         flagsToSkip && flagsToSkip.length ? flagsToSkip : null,
-        useNodeTime ? new Date().toISOString() : null,
+        ...(useNodeTime ? [new Date().toISOString()] : []),
       ],
-      name: noPreparedStatements ? undefined : `get_job/${workerSchema}`,
+      name: noPreparedStatements
+        ? undefined
+        : `get_job${useNodeTime ? "N" : ""}/${workerSchema}`,
     }),
   );
   return jobRow;
