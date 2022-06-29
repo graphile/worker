551e612b0199138228eba0ad7f562851d9d1c221
Benjie Gillam <benjie@jemjie.com>
2022-06-29 18:02:22 +0100

    Update schemas

--- __tests__/schema.sql
+++ __tests__/schema.sql
@@ -1,9 +1,19 @@
 SELECT pg_catalog.set_config('search_path', '', false);
 CREATE SCHEMA graphile_worker;
+CREATE TYPE graphile_worker.job_spec AS (
+	identifier text,
+	payload json,
+	queue_name text,
+	run_at timestamp with time zone,
+	max_attempts integer,
+	job_key text,
+	priority integer,
+	flags text[]
+);
 CREATE TABLE graphile_worker.jobs (
     id bigint NOT NULL,
-    queue_name text,
-    task_identifier text NOT NULL,
+    job_queue_id integer,
+    task_id integer NOT NULL,
     payload json DEFAULT '{}'::json NOT NULL,
     priority smallint DEFAULT 0 NOT NULL,
     run_at timestamp with time zone DEFAULT now() NOT NULL,
@@ -18,7 +28,8 @@ CREATE TABLE graphile_worker.jobs (
     revision integer DEFAULT 0 NOT NULL,
     flags jsonb,
     is_available boolean GENERATED ALWAYS AS (((locked_at IS NULL) AND (attempts < max_attempts))) STORED NOT NULL,
-    CONSTRAINT jobs_key_check CHECK ((length(key) > 0))
+    CONSTRAINT jobs_key_check CHECK (((length(key) > 0) AND (length(key) <= 512))),
+    CONSTRAINT jobs_max_attempts_check CHECK ((max_attempts >= 1))
 );
 CREATE FUNCTION graphile_worker.add_job(identifier text, payload json DEFAULT NULL::json, queue_name text DEFAULT NULL::text, run_at timestamp with time zone DEFAULT NULL::timestamp with time zone, max_attempts integer DEFAULT NULL::integer, job_key text DEFAULT NULL::text, priority integer DEFAULT NULL::integer, flags text[] DEFAULT NULL::text[], job_key_mode text DEFAULT 'replace'::text) RETURNS graphile_worker.jobs
     LANGUAGE plpgsql
@@ -26,146 +37,174 @@ CREATE FUNCTION graphile_worker.add_job(identifier text, payload json DEFAULT NU
 declare
   v_job "graphile_worker".jobs;
 begin
-  -- Apply rationality checks
-  if length(identifier) > 128 then
-    raise exception 'Task identifier is too long (max length: 128).' using errcode = 'GWBID';
-  end if;
-  if queue_name is not null and length(queue_name) > 128 then
-    raise exception 'Job queue name is too long (max length: 128).' using errcode = 'GWBQN';
-  end if;
-  if job_key is not null and length(job_key) > 512 then
-    raise exception 'Job key is too long (max length: 512).' using errcode = 'GWBJK';
-  end if;
-  if max_attempts < 1 then
-    raise exception 'Job maximum attempts must be at least 1.' using errcode = 'GWBMA';
-  end if;
-  if job_key is not null and (job_key_mode is null or job_key_mode in ('replace', 'preserve_run_at')) then
-    -- Upsert job if existing job isn't locked, but in the case of locked
-    -- existing job create a new job instead as it must have already started
-    -- executing (i.e. it's world state is out of date, and the fact add_job
-    -- has been called again implies there's new information that needs to be
-    -- acted upon).
-    insert into "graphile_worker".jobs (
-      task_identifier,
-      payload,
-      queue_name,
-      run_at,
-      max_attempts,
-      key,
-      priority,
-      flags
-    )
-      values(
+  if (job_key_mode is null or job_key_mode in ('replace', 'preserve_run_at')) then
+    select * into v_job
+    from "graphile_worker".add_jobs(
+      ARRAY[(
         identifier,
-        coalesce(payload, '{}'::json),
+        payload,
         queue_name,
-        coalesce(run_at, now()),
-        coalesce(max_attempts, 25),
+        run_at,
+        max_attempts,
         job_key,
-        coalesce(priority, 0),
-        (
-          select jsonb_object_agg(flag, true)
-          from unnest(flags) as item(flag)
-        )
-      )
-      on conflict (key) do update set
-        task_identifier=excluded.task_identifier,
-        payload=excluded.payload,
-        queue_name=excluded.queue_name,
-        max_attempts=excluded.max_attempts,
-        run_at=(case
-          when job_key_mode = 'preserve_run_at' and jobs.attempts = 0 then jobs.run_at
-          else excluded.run_at
-        end),
-        priority=excluded.priority,
-        revision=jobs.revision + 1,
-        flags=excluded.flags,
-        -- always reset error/retry state
-        attempts=0,
-        last_error=null
-      where jobs.locked_at is null
-      returning *
-      into v_job;
-    -- If upsert succeeded (insert or update), return early
-    if not (v_job is null) then
-      return v_job;
-    end if;
-    -- Upsert failed -> there must be an existing job that is locked. Remove
-    -- existing key to allow a new one to be inserted, and prevent any
-    -- subsequent retries of existing job by bumping attempts to the max
-    -- allowed.
-    update "graphile_worker".jobs
-      set
-        key = null,
-        attempts = jobs.max_attempts
-      where key = job_key;
-  elsif job_key is not null and job_key_mode = 'unsafe_dedupe' then
-    -- Insert job, but if one already exists then do nothing, even if the
-    -- existing job has already started (and thus represents an out-of-date
-    -- world state). This is dangerous because it means that whatever state
-    -- change triggered this add_job may not be acted upon (since it happened
-    -- after the existing job started executing, but no further job is being
-    -- scheduled), but it is useful in very rare circumstances for
-    -- de-duplication. If in doubt, DO NOT USE THIS.
-    insert into "graphile_worker".jobs (
-      task_identifier,
-      payload,
-      queue_name,
-      run_at,
-      max_attempts,
-      key,
-      priority,
-      flags
+        priority,
+        flags
+      )::"graphile_worker".job_spec],
+      (job_key_mode = 'preserve_run_at')
     )
-      values(
-        identifier,
-        coalesce(payload, '{}'::json),
-        queue_name,
-        coalesce(run_at, now()),
-        coalesce(max_attempts, 25),
-        job_key,
-        coalesce(priority, 0),
-        (
-          select jsonb_object_agg(flag, true)
-          from unnest(flags) as item(flag)
-        )
-      )
-      on conflict (key)
-      -- Bump the revision so that there's something to return
-      do update set revision = jobs.revision + 1
-      returning *
-      into v_job;
+    limit 1;
     return v_job;
+  elsif job_key is not null and job_key_mode = 'unsafe_dedupe' then
+    return "graphile_worker".add_job_unsafe_dedupe(
+      (
+        identifier,
+        payload,
+        queue_name,
+        run_at,
+        max_attempts,
+        job_key,
+        priority,
+        flags
+      )::"graphile_worker".job_spec
+    );
   elsif job_key is not null then
     raise exception 'Invalid job_key_mode value, expected ''replace'', ''preserve_run_at'' or ''unsafe_dedupe''.' using errcode = 'GWBKM';
   end if;
-  -- insert the new job. Assume no conflicts due to the update above
-  insert into "graphile_worker".jobs(
-    task_identifier,
+end;
+$$;
+CREATE FUNCTION graphile_worker.add_job_unsafe_dedupe(spec graphile_worker.job_spec) RETURNS graphile_worker.jobs
+    LANGUAGE plpgsql STRICT
+    AS $$
+declare
+  v_job "graphile_worker".jobs;
+begin
+  -- Ensure all the tasks exist
+  insert into "graphile_worker".tasks (identifier)
+  select distinct spec.identifier
+  on conflict do nothing;
+  -- Ensure all the queues exist
+  if spec.queue_name is not null then
+    insert into "graphile_worker".job_queues (queue_name)
+    select distinct spec.queue_name
+    on conflict do nothing;
+  end if;
+  -- Insert job, but if one already exists then do nothing, even if the
+  -- existing job has already started (and thus represents an out-of-date
+  -- world state). This is dangerous because it means that whatever state
+  -- change triggered this add_job may not be acted upon (since it happened
+  -- after the existing job started executing, but no further job is being
+  -- scheduled), but it is useful in very rare circumstances for
+  -- de-duplication. If in doubt, DO NOT USE THIS.
+  insert into "graphile_worker".jobs (
+    job_queue_id,
+    task_id,
     payload,
-    queue_name,
     run_at,
     max_attempts,
     key,
     priority,
     flags
   )
-    values(
-      identifier,
-      coalesce(payload, '{}'::json),
-      queue_name,
-      coalesce(run_at, now()),
-      coalesce(max_attempts, 25),
-      job_key,
-      coalesce(priority, 0),
+    select
+      job_queues.id,
+      tasks.id,
+      coalesce(spec.payload, '{}'::json),
+      coalesce(spec.run_at, now()),
+      coalesce(spec.max_attempts, 25),
+      spec.job_key,
+      coalesce(spec.priority, 0),
       (
         select jsonb_object_agg(flag, true)
-        from unnest(flags) as item(flag)
+        from unnest(spec.flags) as item(flag)
       )
-    )
+    from unnest(specs) spec
+    inner join "graphile_worker".tasks
+    on tasks.identifier = spec.identifier
+    left join "graphile_worker".job_queues
+    on job_queues.queue_name = spec.queue_name
+  on conflict (key)
+    -- Bump the updated_at so that there's something to return
+    do update set updated_at = now()
     returning *
     into v_job;
   return v_job;
+end;
+$$;
+CREATE FUNCTION graphile_worker.add_jobs(specs graphile_worker.job_spec[], job_key_preserve_run_at boolean DEFAULT false) RETURNS SETOF graphile_worker.jobs
+    LANGUAGE plpgsql
+    AS $$
+begin
+  -- Ensure all the tasks exist
+  insert into "graphile_worker".tasks (identifier)
+  select distinct spec.identifier
+  from unnest(specs) spec
+  on conflict do nothing;
+  -- Ensure all the queues exist
+  insert into "graphile_worker".job_queues (queue_name)
+  select distinct spec.queue_name
+  from unnest(specs) spec
+  where spec.queue_name is not null
+  on conflict do nothing;
+  -- Ensure any locked jobs have their key cleared - in the case of locked
+  -- existing job create a new job instead as it must have already started
+  -- executing (i.e. it's world state is out of date, and the fact add_job
+  -- has been called again implies there's new information that needs to be
+  -- acted upon).
+  update "graphile_worker".jobs
+  set
+    key = null,
+    attempts = jobs.max_attempts,
+    updated_at = now()
+  from unnest(specs) spec
+  where spec.job_key is not null
+  and jobs.key = spec.job_key;
+  -- TODO: is there a risk that a conflict could occur depending on the
+  -- isolation level?
+  return query insert into "graphile_worker".jobs (
+    job_queue_id,
+    task_id,
+    payload,
+    run_at,
+    max_attempts,
+    key,
+    priority,
+    flags
+  )
+    select
+      job_queues.id,
+      tasks.id,
+      coalesce(spec.payload, '{}'::json),
+      coalesce(spec.run_at, now()),
+      coalesce(spec.max_attempts, 25),
+      spec.job_key,
+      coalesce(spec.priority, 0),
+      (
+        select jsonb_object_agg(flag, true)
+        from unnest(spec.flags) as item(flag)
+      )
+    from unnest(specs) spec
+    inner join "graphile_worker".tasks
+    on tasks.identifier = spec.identifier
+    left join "graphile_worker".job_queues
+    on job_queues.queue_name = spec.queue_name
+  on conflict (key) do update set
+    job_queue_id = excluded.job_queue_id,
+    task_id = excluded.task_id,
+    payload = excluded.payload,
+    max_attempts = excluded.max_attempts,
+    run_at = (case
+      when job_key_preserve_run_at is true and jobs.attempts = 0 then jobs.run_at
+      else excluded.run_at
+    end),
+    priority = excluded.priority,
+    revision = jobs.revision + 1,
+    flags = excluded.flags,
+    -- always reset error/retry state
+    attempts = 0,
+    last_error = null,
+    updated_at = now()
+  where jobs.locked_at is null
+  returning *;
 end;
 $$;
 CREATE FUNCTION graphile_worker.complete_jobs(job_ids bigint[]) RETURNS SETOF graphile_worker.jobs
@@ -174,9 +213,9 @@ CREATE FUNCTION graphile_worker.complete_jobs(job_ids bigint[]) RETURNS SETOF gr
   delete from "graphile_worker".jobs
     where id = any(job_ids)
     and (
-      locked_by is null
+      locked_at is null
     or
-      locked_at < NOW() - interval '4 hours'
+      locked_at < now() - interval '4 hours'
     )
     returning *;
 $$;
@@ -214,10 +253,11 @@ CREATE FUNCTION graphile_worker.permanently_fail_jobs(job_ids bigint[], error_me
   update "graphile_worker".jobs
     set
       last_error = coalesce(error_message, 'Manually marked as failed'),
-      attempts = max_attempts
+      attempts = max_attempts,
+      updated_at = now()
     where id = any(job_ids)
     and (
-      locked_by is null
+      locked_at is null
     or
       locked_at < NOW() - interval '4 hours'
     )
@@ -232,15 +272,22 @@ begin
   -- Delete job if not locked
   delete from "graphile_worker".jobs
     where key = job_key
-    and locked_at is null
+    and (
+      locked_at is null
+    or
+      locked_at < NOW() - interval '4 hours'
+    )
   returning * into v_job;
   if not (v_job is null) then
     return v_job;
   end if;
   -- Otherwise prevent job from retrying, and clear the key
   update "graphile_worker".jobs
-    set attempts = max_attempts, key = null
-    where key = job_key
+  set
+    key = null,
+    attempts = jobs.max_attempts,
+    updated_at = now()
+  where key = job_key
   returning * into v_job;
   return v_job;
 end;
@@ -253,10 +300,11 @@ CREATE FUNCTION graphile_worker.reschedule_jobs(job_ids bigint[], run_at timesta
       run_at = coalesce(reschedule_jobs.run_at, jobs.run_at),
       priority = coalesce(reschedule_jobs.priority, jobs.priority),
       attempts = coalesce(reschedule_jobs.attempts, jobs.attempts),
-      max_attempts = coalesce(reschedule_jobs.max_attempts, jobs.max_attempts)
+      max_attempts = coalesce(reschedule_jobs.max_attempts, jobs.max_attempts),
+      updated_at = now()
     where id = any(job_ids)
     and (
-      locked_by is null
+      locked_at is null
     or
       locked_at < NOW() - interval '4 hours'
     )
@@ -270,7 +318,7 @@ begin
   return new;
 end;
 $$;
-CREATE FUNCTION graphile_worker.tg_jobs__notify_new_jobs() RETURNS trigger
+CREATE FUNCTION graphile_worker.tg_jobs__after_insert() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
 begin
@@ -279,19 +327,29 @@ begin
 end;
 $$;
 CREATE TABLE graphile_worker.job_queues (
+    id integer NOT NULL,
     queue_name text NOT NULL,
-    job_count integer NOT NULL,
     locked_at timestamp with time zone,
     locked_by text,
-    is_available boolean GENERATED ALWAYS AS ((locked_at IS NULL)) STORED NOT NULL
+    is_available boolean GENERATED ALWAYS AS ((locked_at IS NULL)) STORED NOT NULL,
+    CONSTRAINT job_queues_queue_name_check CHECK ((length(queue_name) <= 128))
 );
-CREATE SEQUENCE graphile_worker.jobs_id_seq
+ALTER TABLE graphile_worker.job_queues ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME graphile_worker.job_queues_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
-    CACHE 1;
-ALTER SEQUENCE graphile_worker.jobs_id_seq OWNED BY graphile_worker.jobs.id;
+    CACHE 1
+);
+ALTER TABLE graphile_worker.jobs ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME graphile_worker.jobs_id_seq1
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
 CREATE TABLE graphile_worker.known_crontabs (
     identifier text NOT NULL,
     known_since timestamp with time zone NOT NULL,
@@ -301,25 +359,39 @@ CREATE TABLE graphile_worker.migrations (
     id integer NOT NULL,
     ts timestamp with time zone DEFAULT now() NOT NULL
 );
-ALTER TABLE ONLY graphile_worker.jobs ALTER COLUMN id SET DEFAULT nextval('graphile_worker.jobs_id_seq'::regclass);
+CREATE TABLE graphile_worker.tasks (
+    id integer NOT NULL,
+    identifier text NOT NULL,
+    CONSTRAINT tasks_identifier_check CHECK ((length(identifier) <= 128))
+);
+ALTER TABLE graphile_worker.tasks ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME graphile_worker.tasks_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
 ALTER TABLE ONLY graphile_worker.job_queues
-    ADD CONSTRAINT job_queues_pkey PRIMARY KEY (queue_name);
+    ADD CONSTRAINT job_queues_pkey1 PRIMARY KEY (id);
+ALTER TABLE ONLY graphile_worker.job_queues
+    ADD CONSTRAINT job_queues_queue_name_key UNIQUE (queue_name);
 ALTER TABLE ONLY graphile_worker.jobs
-    ADD CONSTRAINT jobs_key_key UNIQUE (key);
+    ADD CONSTRAINT jobs_key_key1 UNIQUE (key);
 ALTER TABLE ONLY graphile_worker.jobs
-    ADD CONSTRAINT jobs_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT jobs_pkey1 PRIMARY KEY (id);
 ALTER TABLE ONLY graphile_worker.known_crontabs
     ADD CONSTRAINT known_crontabs_pkey PRIMARY KEY (identifier);
 ALTER TABLE ONLY graphile_worker.migrations
     ADD CONSTRAINT migrations_pkey PRIMARY KEY (id);
-CREATE INDEX job_queues_queue_name ON graphile_worker.job_queues USING btree (queue_name) WHERE (is_available = true);
-CREATE INDEX jobs_priority_run_at_id_locked_at_without_failures_idx ON graphile_worker.jobs USING btree (priority, run_at) INCLUDE (id, task_identifier) WHERE (is_available = true);
-CREATE TRIGGER _100_timestamps BEFORE UPDATE ON graphile_worker.jobs FOR EACH ROW EXECUTE PROCEDURE graphile_worker.tg__update_timestamp();
-CREATE TRIGGER _500_decrease_job_queue_count AFTER DELETE ON graphile_worker.jobs FOR EACH ROW WHEN ((old.queue_name IS NOT NULL)) EXECUTE PROCEDURE graphile_worker.jobs__decrease_job_queue_count();
-CREATE TRIGGER _500_decrease_job_queue_count_update AFTER UPDATE OF queue_name ON graphile_worker.jobs FOR EACH ROW WHEN (((new.queue_name IS DISTINCT FROM old.queue_name) AND (old.queue_name IS NOT NULL))) EXECUTE PROCEDURE graphile_worker.jobs__decrease_job_queue_count();
-CREATE TRIGGER _500_increase_job_queue_count AFTER INSERT ON graphile_worker.jobs FOR EACH ROW WHEN ((new.queue_name IS NOT NULL)) EXECUTE PROCEDURE graphile_worker.jobs__increase_job_queue_count();
-CREATE TRIGGER _500_increase_job_queue_count_update AFTER UPDATE OF queue_name ON graphile_worker.jobs FOR EACH ROW WHEN (((new.queue_name IS DISTINCT FROM old.queue_name) AND (new.queue_name IS NOT NULL))) EXECUTE PROCEDURE graphile_worker.jobs__increase_job_queue_count();
-CREATE TRIGGER _900_notify_worker AFTER INSERT ON graphile_worker.jobs FOR EACH STATEMENT EXECUTE PROCEDURE graphile_worker.tg_jobs__notify_new_jobs();
+ALTER TABLE ONLY graphile_worker.tasks
+    ADD CONSTRAINT tasks_identifier_key UNIQUE (identifier);
+ALTER TABLE ONLY graphile_worker.tasks
+    ADD CONSTRAINT tasks_pkey PRIMARY KEY (id);
+CREATE INDEX jobs_main_index ON graphile_worker.jobs USING btree (priority, run_at) INCLUDE (id, task_id, job_queue_id) WHERE (is_available = true);
+CREATE INDEX jobs_no_queue_index ON graphile_worker.jobs USING btree (priority, run_at) INCLUDE (id, task_id) WHERE ((is_available = true) AND (job_queue_id IS NULL));
+CREATE TRIGGER _900_after_insert AFTER INSERT ON graphile_worker.jobs FOR EACH STATEMENT EXECUTE PROCEDURE graphile_worker.tg_jobs__after_insert();
 ALTER TABLE graphile_worker.job_queues ENABLE ROW LEVEL SECURITY;
 ALTER TABLE graphile_worker.jobs ENABLE ROW LEVEL SECURITY;
 ALTER TABLE graphile_worker.known_crontabs ENABLE ROW LEVEL SECURITY;
+ALTER TABLE graphile_worker.tasks ENABLE ROW LEVEL SECURITY;
