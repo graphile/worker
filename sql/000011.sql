dd9043542ded3d4eb7783f7fd6b64c7076b14fa7
Benjie Gillam <benjie@jemjie.com>
2022-06-29 13:37:08 +0100

    Use renamable schema

--- sql/000011.sql
+++ sql/000011.sql
@@ -1,25 +1,25 @@
-drop function graphile_worker.complete_job;
-drop function graphile_worker.fail_job;
-drop function graphile_worker.get_job;
+drop function :GRAPHILE_WORKER_SCHEMA.complete_job;
+drop function :GRAPHILE_WORKER_SCHEMA.fail_job;
+drop function :GRAPHILE_WORKER_SCHEMA.get_job;
 
-alter table graphile_worker.jobs
+alter table :GRAPHILE_WORKER_SCHEMA.jobs
 alter column attempts type int2,
 alter column max_attempts type int2,
 alter column priority type int2;
 
-alter table graphile_worker.jobs add column is_available boolean not null
+alter table :GRAPHILE_WORKER_SCHEMA.jobs add column is_available boolean not null
   generated always as (locked_at is null and attempts < max_attempts) stored;
-alter table graphile_worker.job_queues add column is_available boolean not null
+alter table :GRAPHILE_WORKER_SCHEMA.job_queues add column is_available boolean not null
   generated always as (locked_at is null) stored;
 
 create index job_queues_queue_name
-on graphile_worker.job_queues
+on :GRAPHILE_WORKER_SCHEMA.job_queues
 using btree (queue_name)
 where (is_available = true);
 
-drop index graphile_worker.jobs_priority_run_at_id_locked_at_without_failures_idx;
+drop index :GRAPHILE_WORKER_SCHEMA.jobs_priority_run_at_id_locked_at_without_failures_idx;
 create index jobs_priority_run_at_id_locked_at_without_failures_idx
-on graphile_worker.jobs
+on :GRAPHILE_WORKER_SCHEMA.jobs
 using btree (priority, run_at)
 include (id, task_identifier)
 where (is_available = true);
