a5cef105ce7e257ad53f512bbe0ddc2cb4a39396
Benjie Gillam <benjie@jemjie.com>
2022-06-29 11:31:28 +0100

    Re-inline

--- src/sql/getJob.ts
+++ src/sql/getJob.ts
@@ -22,17 +22,17 @@ export async function getJob(
     : "";
   const now = useNodeTime ? `$${++i}::timestamptz` : "now()";
 
-  const common1 = `select jobs.queue_name, jobs.priority, jobs.run_at, jobs.id
-    from ${escapedWorkerSchema}.jobs
-    where jobs.locked_at is null
-    and run_at <= ${now}
-    and attempts < max_attempts
-    and task_identifier = any($2::text[])
-    ${flagsClause}`;
-  const common2 = `order by priority asc, run_at asc, id asc
-    limit 1
-    for update
-    skip locked`;
+  const queueClause = `and (
+      jobs.queue_name is null
+      or exists (
+        select 1
+        from ${escapedWorkerSchema}.job_queues
+        where job_queues.queue_name = jobs.queue_name
+        and job_queues.locked_at is null
+        for update
+        skip locked
+      )
+    )`;
 
   const {
     rows: [jobRow],
@@ -41,27 +41,19 @@ export async function getJob(
     // `SELECT id, queue_name, task_identifier, payload FROM ...`,
     client.query<Job>({
       text: `\
-with p as (
-  ${common1}
-    and jobs.queue_name is null
-    ${common2}
-union
-  ${common1}
-    and exists (
-      select 1
-      from ${escapedWorkerSchema}.job_queues
-      where job_queues.queue_name = jobs.queue_name
-      and job_queues.locked_at is null
-      for update
-      skip locked
-    )
-    ${common2}
-),
-j as (
-  select *
-  from p
-  order by priority asc, run_at asc, id asc
-  limit 1
+with j as (
+  select jobs.queue_name, jobs.priority, jobs.run_at, jobs.id
+    from ${escapedWorkerSchema}.jobs
+    where jobs.locked_at is null
+    and run_at <= ${now}
+    and attempts < max_attempts
+    and task_identifier = any($2::text[])
+    ${queueClause}
+    ${flagsClause}
+    order by priority asc, run_at asc, id asc
+    limit 1
+    for update
+    skip locked
 ),
 q as (
   update ${escapedWorkerSchema}.job_queues
