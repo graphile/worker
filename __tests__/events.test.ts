5089ee3ab12e04e4542eef1765e1c6cdb3a5c443
Benjie Gillam <benjie@jemjie.com>
2022-06-30 17:43:28 +0100

    chore(tests): make more resilient to deferreds

--- __tests__/events.test.ts
+++ __tests__/events.test.ts
@@ -53,83 +53,87 @@ test("emits the expected events", () =>
     const jobPromises: {
       [id: string]: Deferred | undefined;
     } = {};
-    const job1: Task = jest.fn(({ id }: { id: string }) => {
-      const jobPromise = deferred();
-      if (jobPromises[id]) {
-        throw new Error("Job with this id already registered");
-      }
-      jobPromises[id] = jobPromise;
-      return jobPromise;
-    });
-    const tasks: TaskList = {
-      job1,
-    };
-
-    // Run the worker
-    const events = new EventEmitter();
-
-    const emittedEvents: Array<{ event: string; payload: any }> = [];
-    function createListener(event: string) {
-      return (payload: any) => {
-        emittedEvents.push({ event, payload });
+    try {
+      const job1: Task = jest.fn(({ id }: { id: string }) => {
+        const jobPromise = deferred();
+        if (jobPromises[id]) {
+          throw new Error("Job with this id already registered");
+        }
+        jobPromises[id] = jobPromise;
+        return jobPromise;
+      });
+      const tasks: TaskList = {
+        job1,
       };
+
+      // Run the worker
+      const events = new EventEmitter();
+
+      const emittedEvents: Array<{ event: string; payload: any }> = [];
+      function createListener(event: string) {
+        return (payload: any) => {
+          emittedEvents.push({ event, payload });
+        };
+      }
+
+      EVENTS.forEach((event) => {
+        events.on(event, createListener(event));
+      });
+
+      const CONCURRENCY = 3;
+      const runner = await run({
+        concurrency: CONCURRENCY,
+        pgPool,
+        taskList: tasks,
+        events,
+      });
+
+      expect(runner.events).toEqual(events);
+
+      const eventCount = (name: string) =>
+        emittedEvents.map((obj) => obj.event).filter((n) => n === name).length;
+
+      // NOTE: these are the events that get emitted _before_ `run` resolves; so
+      // you can only receive these if you pass an EventEmitter to run manually.
+      expect(eventCount("pool:create")).toEqual(1);
+      expect(eventCount("pool:listen:connecting")).toEqual(1);
+      expect(eventCount("worker:create")).toEqual(CONCURRENCY);
+
+      let finished = false;
+      runner.promise.then(() => {
+        finished = true;
+      });
+
+      for (let i = 0; i < 5; i++) {
+        await addJob(pgPool, i);
+      }
+
+      for (let i = 0; i < 5; i++) {
+        await sleepUntil(() => !!jobPromises[i]);
+        expect(eventCount("job:start")).toEqual(i + 1);
+        expect(eventCount("job:success")).toEqual(i);
+        expect(eventCount("job:complete")).toEqual(i);
+        jobPromises[i]!.resolve();
+        await sleepUntil(() => eventCount("job:complete") === i + 1);
+        expect(eventCount("job:success")).toEqual(i + 1);
+      }
+
+      await sleep(1);
+      expect(finished).toBeFalsy();
+      expect(eventCount("stop")).toEqual(0);
+      expect(eventCount("worker:release")).toEqual(0);
+      expect(eventCount("pool:release")).toEqual(0);
+      await runner.stop();
+      expect(eventCount("stop")).toEqual(1);
+      expect(job1).toHaveBeenCalledTimes(5);
+      await sleep(1);
+      expect(finished).toBeTruthy();
+      await runner.promise;
+      expect(eventCount("worker:release")).toEqual(CONCURRENCY);
+      expect(eventCount("worker:stop")).toEqual(CONCURRENCY);
+      expect(eventCount("pool:release")).toEqual(1);
+      expect(await jobCount(pgPool)).toEqual(0);
+    } finally {
+      Object.values(jobPromises).forEach((p) => p?.resolve());
     }
-
-    EVENTS.forEach((event) => {
-      events.on(event, createListener(event));
-    });
-
-    const CONCURRENCY = 3;
-    const runner = await run({
-      concurrency: CONCURRENCY,
-      pgPool,
-      taskList: tasks,
-      events,
-    });
-
-    expect(runner.events).toEqual(events);
-
-    const eventCount = (name: string) =>
-      emittedEvents.map((obj) => obj.event).filter((n) => n === name).length;
-
-    // NOTE: these are the events that get emitted _before_ `run` resolves; so
-    // you can only receive these if you pass an EventEmitter to run manually.
-    expect(eventCount("pool:create")).toEqual(1);
-    expect(eventCount("pool:listen:connecting")).toEqual(1);
-    expect(eventCount("worker:create")).toEqual(CONCURRENCY);
-
-    let finished = false;
-    runner.promise.then(() => {
-      finished = true;
-    });
-
-    for (let i = 0; i < 5; i++) {
-      await addJob(pgPool, i);
-    }
-
-    for (let i = 0; i < 5; i++) {
-      await sleepUntil(() => !!jobPromises[i]);
-      expect(eventCount("job:start")).toEqual(i + 1);
-      expect(eventCount("job:success")).toEqual(i);
-      expect(eventCount("job:complete")).toEqual(i);
-      jobPromises[i]!.resolve();
-      await sleepUntil(() => eventCount("job:complete") === i + 1);
-      expect(eventCount("job:success")).toEqual(i + 1);
-    }
-
-    await sleep(1);
-    expect(finished).toBeFalsy();
-    expect(eventCount("stop")).toEqual(0);
-    expect(eventCount("worker:release")).toEqual(0);
-    expect(eventCount("pool:release")).toEqual(0);
-    await runner.stop();
-    expect(eventCount("stop")).toEqual(1);
-    expect(job1).toHaveBeenCalledTimes(5);
-    await sleep(1);
-    expect(finished).toBeTruthy();
-    await runner.promise;
-    expect(eventCount("worker:release")).toEqual(CONCURRENCY);
-    expect(eventCount("worker:stop")).toEqual(CONCURRENCY);
-    expect(eventCount("pool:release")).toEqual(1);
-    expect(await jobCount(pgPool)).toEqual(0);
   }));
