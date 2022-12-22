9ac2c3943fda319f817b4bcb97f2d8c4f19c3266
Max Korp <Maxkorp@8bytealchemy.com>
2022-12-22 11:44:38 -0700

    feat: add Postgres version check (#309)
    Co-authored-by: Benjie <benjie@jemjie.com>

--- src/migrate.ts
+++ src/migrate.ts
@@ -4,8 +4,30 @@ import { readdir, readFile } from "./fs";
 import { WorkerSharedOptions } from "./interfaces";
 import { processSharedOptions } from "./lib";
 
+function checkPostgresVersion(versionString: string) {
+  const version = parseInt(versionString, 10);
+
+  if (version < 120000) {
+    throw new Error(
+      `This version of Graphile Worker requires PostgreSQL v12.0 or greater (detected \`server_version_num\` = ${versionString})`,
+    );
+  }
+}
+
+async function fetchAndCheckPostgresVersion(client: PoolClient) {
+  const {
+    rows: [row],
+  } = await client.query(
+    "select current_setting('server_version_num') as server_version_num",
+  );
+  checkPostgresVersion(row.server_version_num);
+}
+
 async function installSchema(options: WorkerSharedOptions, client: PoolClient) {
   const { escapedWorkerSchema } = processSharedOptions(options);
+
+  await fetchAndCheckPostgresVersion(client);
+
   await client.query(`
     create schema ${escapedWorkerSchema};
     create table ${escapedWorkerSchema}.migrations(
@@ -56,11 +78,12 @@ export async function migrate(
     const {
       rows: [row],
     } = await client.query(
-      `select id from ${escapedWorkerSchema}.migrations order by id desc limit 1;`,
+      `select current_setting('server_version_num') as server_version_num,
+      (select id from ${escapedWorkerSchema}.migrations order by id desc limit 1) as id;`,
     );
-    if (row) {
-      latestMigration = row.id;
-    }
+
+    latestMigration = row.id;
+    checkPostgresVersion(row.server_version_num);
   } catch (e) {
     if (e.code === "42P01") {
       await installSchema(options, client);
@@ -68,6 +91,7 @@ export async function migrate(
       throw e;
     }
   }
+
   const migrationFiles = (await readdir(`${__dirname}/../sql`))
     .filter((f) => f.match(/^[0-9]{6}\.sql$/))
     .sort();
