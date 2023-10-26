3bd9e7262e30878d6fd5de9d4f5e33666f37ec4e
Benjie Gillam <benjie@jemjie.com>
2023-10-26 15:49:28 +0100

    Update DB dump

--- __tests__/schema.sql
+++ __tests__/schema.sql
@@ -314,6 +314,25 @@ ALTER TABLE graphile_worker._private_job_queues ALTER COLUMN id ADD GENERATED AL
     NO MAXVALUE
     CACHE 1
 );
+CREATE VIEW graphile_worker.jobs AS
+ SELECT jobs.id,
+    job_queues.queue_name,
+    tasks.identifier AS task_identifier,
+    jobs.priority,
+    jobs.run_at,
+    jobs.attempts,
+    jobs.max_attempts,
+    jobs.last_error,
+    jobs.created_at,
+    jobs.updated_at,
+    jobs.key,
+    jobs.locked_at,
+    jobs.locked_by,
+    jobs.revision,
+    jobs.flags
+   FROM ((graphile_worker._private_jobs jobs
+     JOIN graphile_worker._private_tasks tasks ON ((tasks.id = jobs.task_id)))
+     LEFT JOIN graphile_worker._private_job_queues job_queues ON ((job_queues.id = jobs.job_queue_id)));
 ALTER TABLE graphile_worker._private_jobs ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
     SEQUENCE NAME graphile_worker.jobs_id_seq1
     START WITH 1
