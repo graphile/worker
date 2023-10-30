8595a6b76d6ee237fa9c6835a93c677bff91bdec
Benjie Gillam <benjie@jemjie.com>
2023-10-30 14:47:57 +0000

    Don't assume ts-node is running by default

--- perfTest/graphile.config.ts
+++ perfTest/graphile.config.ts
@@ -6,7 +6,8 @@ const preset: GraphileConfig.Preset = {
   worker: {
     connectionString: "postgres:///graphile_worker_test",
     concurrentJobs: 3,
-    fileExtensions: [".js", ".cjs", ".mjs", ".ts", ".cts", ".mts"],
+    fileExtensions: [".js", ".cjs", ".mjs"],
+    // fileExtensions: [".js", ".cjs", ".mjs", ".ts", ".cts", ".mts"],
   },
 };
 export default preset;
