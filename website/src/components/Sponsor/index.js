75d8988bd940d10553a40275e7a4ad1f5b96a572
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2023-11-30 14:14:49 +0000

    Sponsors block mirrors main website

--- website/src/components/Sponsor/index.js
+++ website/src/components/Sponsor/index.js
@@ -1,15 +1,43 @@
 import Link from "@docusaurus/Link";
 import styles from "@site/src/components/Sponsor/styles.module.css";
 import clsx from "clsx";
-import React from "react";
+import React, { useContext } from "react";
+import { SponsorContext } from "../../contexts/sponsor";
 
-export default function Sponsor({ name, avatar, href }) {
+export default function Sponsor({
+  name,
+  avatar,
+  href,
+  contributor,
+  business,
+  plain,
+}) {
+  const level = useContext(SponsorContext);
+  const showAvatar = level === "featured" || level === "leaders";
   return (
-    <div className={clsx("", styles.sponsor)}>
-      <div>
-        <img src={avatar} />
-        <a href={href}>{name}</a>
-      </div>
+    <div
+      className={clsx(
+        "",
+        styles.sponsor,
+        styles[level],
+        contributor ? styles.contributor : null,
+        business ? styles.business : null,
+        plain ? styles.plain : null,
+      )}
+    >
+      {showAvatar ? (
+        <img
+          className={styles.avatar}
+          src={"https://www.graphile.org" + avatar}
+        />
+      ) : null}
+
+      <Link
+        className={styles.name}
+        to={href ?? "https://www.graphile.org/sponsor"}
+      >
+        {name}
+      </Link>
     </div>
   );
 }
--- website/src/components/Sponsors/index.js
+++ website/src/components/Sponsors/index.js
@@ -1,7 +1,14 @@
 import styles from "@site/src/components/Sponsors/styles.module.css";
 import clsx from "clsx";
 import React from "react";
+import { SponsorContext } from "../../contexts/sponsor";
 
-export default function Sponsors({ children }) {
-  return <div className={clsx("", styles.sponsorContainer)}>{children}</div>;
+export default function Sponsors({ children, level }) {
+  return (
+    <SponsorContext.Provider value={level}>
+      <div className={clsx("", styles.sponsorContainer, styles[level])}>
+        {children}
+      </div>
+    </SponsorContext.Provider>
+  );
 }
