ade7099cb0890f4f1164bc57937da88c528ae310
Benjie Gillam <benjie@jemjie.com>
2024-01-17 16:54:10 +0000

    Test for queue name

---
+++ __tests__/runner.helpers.getTaskName.test.ts
@@ -0,0 +1,158 @@
+import { Pool, PoolClient } from "pg";
+
+import { Job, Runner, RunnerOptions } from "../src/interfaces";
+import { run } from "../src/runner";
+import { TEST_CONNECTION_STRING, sleepUntil } from "./helpers";
+
+let pgPool!: Pool;
+let runner: Runner | null = null;
+
+const JOB_COUNT = 10;
+beforeAll(() => {
+  pgPool = new Pool({
+    connectionString: TEST_CONNECTION_STRING,
+    max: JOB_COUNT * 2 + 5,
+  });
+});
+afterAll(() => {
+  pgPool.end();
+});
+
+afterEach(async () => {
+  if (runner) {
+    await runner.stop();
+    runner = null;
+  }
+});
+
+test("getTaskName works as expected", async () => {
+  let results: Record<string, string | null> = Object.create(null);
+
+  const options: RunnerOptions = {
+    pgPool,
+    maxPoolSize: JOB_COUNT * 2 + 5,
+    concurrency: JOB_COUNT * 2,
+    taskList: {
+      async job1(payload, helpers) {
+        const queueNamePromise = helpers.getQueueName();
+        const queueName = await queueNamePromise;
+        results[payload.id] =
+          typeof queueNamePromise === "string" || queueName === null
+            ? `CACHE<${queueName}>`
+            : `FETCH<${queueName}>`;
+      },
+    },
+  };
+  runner = await run(options);
+
+  // Warmup pool
+  {
+    const promises: Promise<PoolClient>[] = [];
+    for (let i = 0; i < JOB_COUNT * 2 + 1; i++) {
+      promises.push(pgPool.connect());
+    }
+    const clients = await Promise.all(promises);
+    clients.forEach((client) => client.release());
+  }
+
+  // First set of tests
+  {
+    const promises: Promise<Job>[] = [];
+    for (let i = 1; i <= JOB_COUNT; i++) {
+      promises.push(
+        runner.addJob(
+          "job1",
+          { id: `j${i}` },
+          {
+            queueName: `q${i % 7}`,
+            runAt: new Date(Date.now() - (JOB_COUNT - i) * 60 * 1000),
+          },
+        ),
+      );
+    }
+    await Promise.all(promises);
+  }
+  await sleepUntil(() => Object.keys(results).length === JOB_COUNT);
+  expect(results).toEqual({
+    j1: "FETCH<q1>",
+    j2: "FETCH<q2>",
+    j3: "FETCH<q3>",
+    j4: "FETCH<q4>",
+    j5: "FETCH<q5>",
+    j6: "FETCH<q6>",
+    j7: "FETCH<q0>",
+    // Same queue as before, so runs later
+    j8: "CACHE<q1>",
+    j9: "CACHE<q2>",
+    j10: "CACHE<q3>",
+  });
+
+  // Do it again; shouldn't need a DB lookup
+  results = Object.create(null);
+  {
+    const promises: Promise<Job>[] = [];
+    for (let i = 1; i <= JOB_COUNT; i++) {
+      promises.push(
+        runner.addJob(
+          "job1",
+          { id: `j${i}` },
+          {
+            queueName: `q${i % 7}`,
+            runAt: new Date(Date.now() - (JOB_COUNT - i) * 60 * 1000),
+          },
+        ),
+      );
+    }
+    await Promise.all(promises);
+  }
+  await sleepUntil(() => Object.keys(results).length === JOB_COUNT);
+  expect(results).toEqual({
+    // All of these are already known
+    j1: "CACHE<q1>",
+    j2: "CACHE<q2>",
+    j3: "CACHE<q3>",
+    j4: "CACHE<q4>",
+    j5: "CACHE<q5>",
+    j6: "CACHE<q6>",
+    j7: "CACHE<q0>",
+    j8: "CACHE<q1>",
+    j9: "CACHE<q2>",
+    j10: "CACHE<q3>",
+  });
+
+  // Mixture of old and new queues
+  results = Object.create(null);
+  {
+    const promises: Promise<Job>[] = [];
+    for (let i = 1; i <= JOB_COUNT; i++) {
+      promises.push(
+        runner.addJob(
+          "job1",
+          { id: `j${i}` },
+          {
+            queueName: `q${(i % 7) + 5}`,
+            runAt: new Date(Date.now() - (JOB_COUNT - i) * 60 * 1000),
+          },
+        ),
+      );
+    }
+    await Promise.all(promises);
+  }
+  await sleepUntil(() => Object.keys(results).length === JOB_COUNT);
+  expect(results).toEqual({
+    // Already known
+    j1: "CACHE<q6>",
+    // New
+    j2: "FETCH<q7>",
+    j3: "FETCH<q8>",
+    j4: "FETCH<q9>",
+    j5: "FETCH<q10>",
+    j6: "FETCH<q11>",
+    // Already known
+    j7: "CACHE<q5>",
+    j8: "CACHE<q6>",
+    // Same queue, so runs later
+    j9: "CACHE<q7>",
+    j10: "CACHE<q8>",
+  });
+});
