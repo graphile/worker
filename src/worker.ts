1821691d9cde6328527aae5c7fcc3739dea0b561
Benjie Gillam <benjie@jemjie.com>
2024-06-06 10:36:36 +0100

    Evaluate envvar just once up top

--- src/worker.ts
+++ src/worker.ts
@@ -19,6 +19,8 @@ import { coerceError, CompiledSharedOptions } from "./lib";
 
 const NO_LOG_SUCCESS = !!process.env.NO_LOG_SUCCESS;
 
+const NO_LOG_SUCCESS = !!process.env.NO_LOG_SUCCESS;
+
 export function makeNewWorker(
   compiledSharedOptions: CompiledSharedOptions<WorkerSharedOptions>,
   params: {
