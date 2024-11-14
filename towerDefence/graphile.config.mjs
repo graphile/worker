24d51c178a9cd7b22c6b176ce0dd85e5aac4fc6b
Benjie Gillam <benjie@jemjie.com>
2024-11-14 15:24:57 +0000

    Easier to read results

--- towerDefence/graphile.config.mjs
+++ towerDefence/graphile.config.mjs
@@ -10,7 +10,7 @@ const CONCURRENT_JOBS = 10;
 const stats = {
   jobsFetched: 0,
   jobsReturned: 0,
-  timeInMode: {},
+  timeInMode: Object.create(null),
   timeInRefetchDelay: 0n,
   refetchDelays: 0,
   refetchDelayAborted: 0,
@@ -18,6 +18,23 @@ const stats = {
 
 let lastModeStart = process.hrtime.bigint();
 let refetchDelayStart = process.hrtime.bigint();
+
+/** @type {(value: number | string, width?: number, char?: string) => string} */
+const p = (v, w = 10, s = " ") => String(v).padStart(w, s);
+
+/** @type {(t: bigint) => string} */
+const ms = (t) => {
+  return `${(Number(t) / 1e6).toFixed(2)}ms`;
+};
+
+/** @type {() => string} */
+const tim = () => {
+  let results = [];
+  for (const m in stats.timeInMode) {
+    results.push(p(`${p(m)}=${ms(stats.timeInMode[m])}`, 19));
+  }
+  return results.join(",");
+};
 
 /** @type {GraphileConfig.Plugin} */
 const TowerDefenceResultPlugin = {
@@ -27,8 +44,18 @@ const TowerDefenceResultPlugin = {
     hooks: {
       init(ctx) {
         ctx.events.on("pool:release", (event) => {
-          console.log(`Pool ${event.workerPool.id} released`);
-          console.dir(stats);
+          console.log(
+            `\nPool ${event.workerPool.id} released\nFetched=${p(
+              stats.jobsFetched,
+              6,
+            )}|Returned=${p(stats.jobsReturned, 6)}|TotalDelay=${p(
+              ms(stats.timeInRefetchDelay),
+              11,
+            )}(Aborted=${p(
+              `${stats.refetchDelayAborted}/${stats.refetchDelays}`,
+              9,
+            )})|${tim()}\n`,
+          );
         });
         ctx.events.on("localQueue:getJobs:complete", ({ jobs }) => {
           stats.jobsFetched += jobs.length;
@@ -81,7 +108,7 @@ const preset = {
     //completeJobBatchDelay: -1,
     //failJobBatchDelay: -1,
 
-    localQueue: { size: 500, refetchDelay: { durationMs: 10 } },
+    localQueue: { size: 500, refetchDelay: { durationMs: 100 } },
     completeJobBatchDelay: 0,
     failJobBatchDelay: 0,
   },
