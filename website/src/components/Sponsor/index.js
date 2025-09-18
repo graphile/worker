92461811a54a77d5ed2a01224a91ad5be9ef9a5d
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2025-09-18 11:07:17 +0100

    Update date and color contrast of dev support boxes

--- website/src/components/Sponsor/index.js
+++ website/src/components/Sponsor/index.js
@@ -35,24 +35,6 @@ export default function Sponsor({
   const level = useContext(SponsorContext);
   const showAvatar = level === "featured" || level === "leaders";
 
-  function Avatar({ src }) {
-    return src ? (
-      <img className={styles.avatar} src={src} />
-    ) : (
-      <FallbackAvatar className={styles.avatar} />
-    );
-  }
-
-  function LinkTo({ href }) {
-    return href ? (
-      <Link className={styles.name} to={href}>
-        {name}
-      </Link>
-    ) : (
-      <div className={styles.name}>{name}</div>
-    );
-  }
-
   return (
     <div
       className={clsx(
@@ -68,7 +50,7 @@ export default function Sponsor({
         <Avatar src={avatar ? "https://www.graphile.org" + avatar : null} />
       ) : null}
 
-      <LinkTo href={href} />
+      <LinkTo href={href} name={name} />
     </div>
   );
 }
