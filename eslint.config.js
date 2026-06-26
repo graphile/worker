e7ce92f3a18d33085e7be3324143305b1bb4f8d4
Benjie Gillam <benjie@jemjie.com>
2026-06-26 17:03:30 +0100

    Formatting

--- eslint.config.js
+++ eslint.config.js
@@ -21,117 +21,112 @@ const ignores = readFileSync(`${__dirname}/.eslintignore`, "utf8")
   })
   .filter((l) => l != null);
 
-// prettier-ignore
 module.exports = [
   {
-    ignores: [
-      ...ignores,
-      "eslint.config.js",
-      ".prettierrc.js",
-    ],
+    ignores: [...ignores, "eslint.config.js", ".prettierrc.js"],
   },
   ...compat
     .config({
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/errors",
-    "plugin:import/typescript",
-    "prettier",
-  ],
-  plugins: ["jest", "@typescript-eslint", "simple-import-sort", "import"],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  env: {
-    node: true,
-    jest: true,
-    es6: true,
-  },
-  globals: {
-    NodeJS: false, // For TypeScript
-  },
-  rules: {
-    "no-unused-vars": 0,
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        caughtErrors: "none",
-        ignoreRestSiblings: true,
+      parser: "@typescript-eslint/parser",
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/errors",
+        "plugin:import/typescript",
+        "prettier",
+      ],
+      plugins: ["jest", "@typescript-eslint", "simple-import-sort", "import"],
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
       },
-    ],
-    curly: "error",
-    "no-else-return": 0,
-    "no-return-assign": [2, "except-parens"],
-    "no-underscore-dangle": 0,
-    "jest/no-focused-tests": 2,
-    "jest/no-identical-title": 2,
-    camelcase: 0,
-    "prefer-arrow-callback": [
-      "error",
-      {
-        allowNamedFunctions: true,
+      env: {
+        node: true,
+        jest: true,
+        es6: true,
       },
-    ],
-    "class-methods-use-this": 0,
-    "no-restricted-syntax": 0,
-    "no-param-reassign": [
-      "error",
-      {
-        props: false,
+      globals: {
+        NodeJS: false, // For TypeScript
       },
-    ],
-
-    "arrow-body-style": 0,
-    "no-nested-ternary": 0,
-
-    /*
-     * simple-import-sort seems to be the most stable import sorting currently,
-     * disable others
-     */
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "sort-imports": "off",
-    "import/order": "off",
-
-    "import/no-deprecated": "warn",
-    "import/no-duplicates": "error",
-    // Doesn't support 'exports'?
-    "import/no-unresolved": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-namespace": "off",
-
-    // ESLint 9 additions
-    "import/namespace": "off",
-    "@typescript-eslint/no-empty-object-type": "off",
-    "@typescript-eslint/no-require-imports": "off",
-  },
-  overrides: [
-    {
-      files: ["__tests__/**/*", "test.js"],
       rules: {
-        "@typescript-eslint/no-explicit-any": 0,
-        "@typescript-eslint/explicit-function-return-type": 0,
-        "@typescript-eslint/no-var-requires": 0,
-        "@typescript-eslint/ban-ts-comment": 0,
+        "no-unused-vars": 0,
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            args: "after-used",
+            caughtErrors: "none",
+            ignoreRestSiblings: true,
+          },
+        ],
+        curly: "error",
+        "no-else-return": 0,
+        "no-return-assign": [2, "except-parens"],
+        "no-underscore-dangle": 0,
+        "jest/no-focused-tests": 2,
+        "jest/no-identical-title": 2,
+        camelcase: 0,
+        "prefer-arrow-callback": [
+          "error",
+          {
+            allowNamedFunctions: true,
+          },
+        ],
+        "class-methods-use-this": 0,
+        "no-restricted-syntax": 0,
+        "no-param-reassign": [
+          "error",
+          {
+            props: false,
+          },
+        ],
+
+        "arrow-body-style": 0,
+        "no-nested-ternary": 0,
+
+        /*
+         * simple-import-sort seems to be the most stable import sorting currently,
+         * disable others
+         */
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+        "sort-imports": "off",
+        "import/order": "off",
+
+        "import/no-deprecated": "warn",
+        "import/no-duplicates": "error",
+        // Doesn't support 'exports'?
+        "import/no-unresolved": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/no-namespace": "off",
 
         // ESLint 9 additions
-        "@typescript-eslint/no-unused-expressions": 0,
+        "import/namespace": "off",
+        "@typescript-eslint/no-empty-object-type": "off",
+        "@typescript-eslint/no-require-imports": "off",
       },
-    },
-    {
-      files: ["perfTest/**/*", "examples/**/*"],
-      rules: {
-        "@typescript-eslint/no-var-requires": 0,
-      },
-    },
-  ],
+      overrides: [
+        {
+          files: ["__tests__/**/*", "test.js"],
+          rules: {
+            "@typescript-eslint/no-explicit-any": 0,
+            "@typescript-eslint/explicit-function-return-type": 0,
+            "@typescript-eslint/no-var-requires": 0,
+            "@typescript-eslint/ban-ts-comment": 0,
+
+            // ESLint 9 additions
+            "@typescript-eslint/no-unused-expressions": 0,
+          },
+        },
+        {
+          files: ["perfTest/**/*", "examples/**/*"],
+          rules: {
+            "@typescript-eslint/no-var-requires": 0,
+          },
+        },
+      ],
     })
     .map((config) => ({
       ...config,
